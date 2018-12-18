d727011a62df0d87805214bee19bfde851be2099
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-12-18 18:22:12 -0500

    Add User Entity, Bootstrap, Font-Awesome

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,8 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +47,7 @@ namespace DatingApp.API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();            
         }
     }
