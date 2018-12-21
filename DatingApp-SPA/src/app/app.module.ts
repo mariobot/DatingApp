3407407bf1696dad19b04cbdc39743ec075f1bd8
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-12-21 09:03:35 -0500

    Add member-card component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -12,13 +12,14 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { ValueComponent } from './value/value.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 @NgModule({
    declarations: [
@@ -29,7 +30,8 @@ import { ValueComponent } from './value/value.component';
       MemberListComponent,
       ListsComponent,
       MessagesComponent,
-      ValueComponent
+      ValueComponent,
+      MemberCardComponent
    ],
    imports: [
       BrowserModule,
