6fa36b747692f50388ffafc1f1159d41c95b53af
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-12-26 09:30:48 -0500

    Using Route Resolvers To retrive data

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-list',
@@ -14,21 +15,26 @@ export class MemberListComponent implements OnInit {
   users: User[];
   values: any;
 
-  constructor(private userService: UserService, private alertify: AlertifyService, private http: HttpClient) { }
+  constructor(private userService: UserService, private alertify: AlertifyService,
+    private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.loadUsers();
+    this.route.data.subscribe(data => {
+      this.users = data['users'];
+    });
+
+    // this.loadUsers();
     // this.getValues();
   }
 
-  loadUsers() {
+  /*loadUsers() {
     this.userService.getUsers().subscribe((users: User[]) => {
       this.users = users;
       console.log(this.users);
     }, error => {
       this.alertify.error(error);
     });
-  }
+  }*/
 
   getValues() {
     this.http.get('http://localhost:5000/api/values/').subscribe(response => {
